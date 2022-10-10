7c7f4ee6cb3b51c5ea7b5fc1ac921cf4a1522da2
URK21CS3015Megha <109952064+URK21CS3015Megha@users.noreply.github.com>
2022-10-10 14:40:07 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from typing import List
 
 def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+  
   x = nums1[0:m]
   y = nums2[0:n]
   nums1=x+y
@@ -28,10 +29,12 @@ def merge_sort(myList) -> None:
             if left[i] <= right[j]:
               # The value from the left half has been used
               myList[k] = left[i]
+              # Move the iterator forward
               i += 1
             else:
                 myList[k] = right[j]
                 j += 1
+            # Move to the next slot
             k += 1
 
         # For all the remaining values
@@ -45,6 +48,7 @@ def merge_sort(myList) -> None:
             j += 1
             k += 1
   return myList
+
 
 # Do not change the following code
 nums1 = []
